6c3dfbe0c2534bccaacba66fa73a4767d58262f4
Ellen Squires <squirese@uw.edu>
2015-07-26 16:15:38 -0700

    Turning in cache matrix Programming Assignment #2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##makeCacheMatrix: The input to this function is an invertible, square matrix. It will return a list of functions that will set
+## the matrix, get the matrix, set the inverse, and get the inverse. This list will be used as the input to the function below, cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv = NULL
+  set = function(y) {
+    # use `<<-` to assign a value to an object in an environment 
+    # different from the current environment. 
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
+##cacheSolve: The output of the above function will be used as the input to this function. This function will return 
+## the inverse of the matrix that was orginially the input to the function above, makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv = x$getinv()
+  
+  if (!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  x$setinv(inv)
+  
+  return(inv)
 }
